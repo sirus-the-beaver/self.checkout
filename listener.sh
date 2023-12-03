6f1771c8c967e902eb2f6aec8d6eb24dd09bff4b
sirus-the-beaver <salaris@oregonstate.edu>
2023-12-02 18:58:12 -0800

    Fix script to watch for when a file is moved to the target directory (     instead of when a file is created in the directory )

--- listener.sh
+++ listener.sh
@@ -1,9 +1,9 @@
-training_directory="/training"
+training_directory="training/sirus_salari"
 
 while true; do
-    file=$(inotifywait -e create --format "%f" "$training_directory")
-    if [[ "$file" == *.jpg ]]; then
-        python3 "facial_detection.py"
+    file=$(inotifywait -e moved_to --format "%f" "$training_directory")
+    if [[ "$file" == scan.jpg ]]; then
+        python3 "facial_detection.py" "IMG_0378.MOV"
         break  # Exit the loop after processing one file
     fi
 done
