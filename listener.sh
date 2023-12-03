2f1b28d17ca454bae1cb7a7153b930cfdba38ee0
sirus-the-beaver <salaris@oregonstate.edu>
2023-12-03 13:24:45 -0800

    Move python command below while loop to fix async issue causing the model to not train with the correct label

--- listener.sh
+++ listener.sh
@@ -3,7 +3,9 @@ training_directory="training/sirus_salari"
 while true; do
     file=$(inotifywait -e moved_to --format "%f" "$training_directory")
     if [[ "$file" == scan.jpg ]]; then
-        python3 "facial_detection.py" "IMG_0378.MOV"
+        echo "File added"
         break  # Exit the loop after processing one file
     fi
 done
+
+python3 "facial_detection.py" "IMG_0378.MOV"
